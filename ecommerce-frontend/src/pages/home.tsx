37dd2ca7f93c96cba5e4e974707377c7f68238dc
Bodhisatwa Chakraborty <bodhisatwachakraborty2002@gmail.com>
2024-02-12 16:19:44 +0530

    added product card

--- ecommerce-frontend/src/pages/home.tsx
+++ ecommerce-frontend/src/pages/home.tsx
@@ -1,7 +1,30 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
 
-export default Home
+const Home = () => {
+  const addToCartHandler = () => {};
+  return (
+    <div className="home">
+      <section></section>
+
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore">
+          More
+        </Link>
+      </h1>
+      <main>
+        <ProductCard
+          productId="adsfs"
+          name="Macbook"
+          price={232}
+          stock={13}
+          handler={addToCartHandler}
+          photo="https://m.media-amazon.com/images/W/MEDIAX_849526-T2/images/I/71TPda7cwUL._SX679_.jpg"
+        />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
