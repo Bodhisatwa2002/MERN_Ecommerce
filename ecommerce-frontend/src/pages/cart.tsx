7cb89c366deda5d0461eb1955ab1e259bad7708f
Bodhisatwa Chakraborty <bodhisatwachakraborty2002@gmail.com>
2024-02-13 00:38:14 +0530

    adding cart and cart-items

--- ecommerce-frontend/src/pages/cart.tsx
+++ ecommerce-frontend/src/pages/cart.tsx
@@ -1,8 +1,76 @@
+import { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc";
+import CartItem from "../components/cart-items";
+
+const cartItems = [
+  {
+    productId: "adsada",
+    photo : "https://m.media-amazon.com/images/W/MEDIAX_849526-T2/images/I/71TPda7cwUL._SX679_.jpg",
+    name:"Macbook",
+    price : 3000,
+    quantity:40,
+    stock:10,
+
+  },
+];
+const subtotal = 4000;
+const tax = Math.round(subtotal * 0.18);
+const shippingCharges = 200;
+const discount = 400;
+const total = subtotal + tax + shippingCharges;
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const [couponCode, setCouponCode] = useState<string>("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
+  useEffect(() => {
+    const timeOutID = setTimeout(() => {
+      if (Math.random() > 0.5) setIsValidCouponCode(true);
+      else setIsValidCouponCode(false);
+    }, 1000);
 
-export default Cart
+    return () => {
+      clearTimeout(timeOutID);
+      setIsValidCouponCode(false);
+    };
+  }, [couponCode]);
+
+  return (
+    <div className="cart">
+      <main>
+        {cartItems.map((i, idx) => (
+          <CartItem  key={idx} cartItem={i}/>
+        ))}
+      </main>
+      <aside>
+        <p> Subtotal: ${subtotal}</p>
+        <p> Shipping Charges: ${shippingCharges}</p>
+        <p> Tax: ${tax}</p>
+        <p>
+          Discount: <em> - ${discount}</em>
+        </p>
+        <p>
+          <b>Total: $ {total}</b>
+        </p>
+        <input
+          placeholder="Coupon Code"
+          type="text"
+          value={couponCode}
+          onChange={(e) => setCouponCode(e.target.value)}
+        />
+
+        {couponCode &&
+          (isValidCouponCode ? (
+            <span className="green">
+              ${discount} off using the <code>{couponCode}</code>
+            </span>
+          ) : (
+            <span className="red">
+              Invalid Coupon <VscError />
+            </span>
+          ))}
+      </aside>
+    </div>
+  );
+};
+
+export default Cart;
