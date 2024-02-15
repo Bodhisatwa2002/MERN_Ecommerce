1e8a99f2494bf38759fff3aa01d1cdf64690ad1d
Bodhisatwa Chakraborty <bodhisatwachakraborty2002@gmail.com>
2024-02-15 18:26:14 +0530

    login page with styles

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -6,6 +6,7 @@ const Home = lazy(() => import("./pages/home"));
 const Search = lazy(() => import("./pages/search"));
 const Cart = lazy(() => import("./pages/cart"));
 const Shipping = lazy(() => import("./pages/shipping"));
+const Login = lazy(() => import("./pages/login"));
 
 // Admin dashboard
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -36,6 +37,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
+          {/* Not logged In Route */}
+          <Route path="/login" element={<Login />} />
 
           {/* Logged In User Routes */}
           <Route>
