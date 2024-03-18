d544a169a874587545971789f149020ac52cd45f
Bodhisatwa Chakraborty <bodhisatwachakraborty2002@gmail.com>
2024-03-18 22:58:18 +0530

    User Schema and Route Setup

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
+
+// Importing Routes
+import userRoute from "./routes/user.js";
 const port = 4000;
 const app = express();
 
+// Using Routes
+app.use("/api/v1/user", userRoute);
+
 app.listen(port, () => {
-  console.log(`Express is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
