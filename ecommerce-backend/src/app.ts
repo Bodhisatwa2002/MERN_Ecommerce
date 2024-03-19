c7db298d530e017d8c88dcdff73d8d33f68a4a00
Bodhisatwa Chakraborty <bodhisatwachakraborty2002@gmail.com>
2024-03-19 23:25:47 +0530

    Handling Errors

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 // Importing Routes
 import userRoute from "./routes/user.js";
 import { connectDB } from "./utils/features.js";
+import { errorMiddleware } from "./middlewares/error.js";
 const port = 4000;
 connectDB();
 const app = express();
@@ -15,6 +16,8 @@ app.get("/", (req, res) => {
 
 // Using Routes
 app.use("/api/v1/user", userRoute);
+
+app.use(errorMiddleware);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
--- ecommerce-backend/src/controllers/user.ts
+++ ecommerce-backend/src/controllers/user.ts
@@ -1,30 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 import { User } from "../models/user.js";
 import { NewUserRequestBody } from "../types/types.js";
-export const newUser = async (
-  req: Request<{}, {}, NewUserRequestBody>,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
+import ErrorHandler from "../utils/utility-class.js";
+import { TryCatch } from "../middlewares/error.js";
+export const newUser = TryCatch(
+  async (
+    req: Request<{}, {}, NewUserRequestBody>,
+    res: Response,
+    next: NextFunction
+  ) => {
     const { name, email, photo, gender, _id, dob } = req.body;
 
     const user = await User.create({
       name,
       email,
       photo,
-      gender, 
+      gender,
       _id,
-      dob:new Date(dob),
+      dob: new Date(dob),
     });
     return res.status(201).json({
       success: true,
       message: `Welcome, ${user.name} `,
     });
-  } catch (error) {
-    return res.status(400).json({
-      success: false,
-      message: error,
-    });
   }
-};
+);
