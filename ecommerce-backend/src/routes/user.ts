98326f30d22c37eb2030cd5ab8a35dd23befd002
Bodhisatwa Chakraborty <bodhisatwachakraborty2002@gmail.com>
2024-03-20 01:40:26 +0530

    User API's, add new user, delete user, update user

--- ecommerce-backend/src/routes/user.ts
+++ ecommerce-backend/src/routes/user.ts
@@ -1,10 +1,19 @@
 import express from "express";
-import { newUser } from "../controllers/user.js";
+import {
+  deleteUser,
+  getAllUsers,
+  getUser,
+  newUser,
+} from "../controllers/user.js";
 
-const app= express.Router();
-
+const app = express.Router();
 
 //route - /api/v1/user/new
- app.post("/new",newUser)
+app.post("/new", newUser);
 
- export default app;
+//route - /api/v1/user/all
+app.get("/all", getAllUsers);
+
+//route - /api/v1/user/dynamicID
+app.route("/:id").get(getUser).delete(deleteUser);
+export default app;
